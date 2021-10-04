385de3c3efb031e2f6f2d8b4a5b922e9cd4d40ee
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-04 12:22:03 -0700

    filled out the ADD_USER mutation and imported it into SignupForm.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,11 +12,20 @@ export const LOGIN_USER = gql`
     }
 `;
 
-// export const ADD_USER = gql`
-//     mutation () {
-        
-//     }
-// `;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password!) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks
+            }
+        }
+    }
+`;
 
 // export const SAVE_BOOK = gql`
 //     mutation () {
