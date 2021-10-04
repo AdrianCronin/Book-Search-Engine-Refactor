ad937290d0232a06707283c2dc5469f1e868c0f0
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-03 17:33:30 -0700

    changing instances of loginUser to LOGIN_USER to start testing new login path

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,20 +12,20 @@ export const LOGIN_USER = gql`
     }
 `;
 
-export const ADD_USER = gql`
-    mutation () {
+// export const ADD_USER = gql`
+//     mutation () {
         
-    }
-`;
+//     }
+// `;
 
-export const SAVE_BOOK = gql`
-    mutation () {
+// export const SAVE_BOOK = gql`
+//     mutation () {
         
-    }
-`;
+//     }
+// `;
 
-export const REMOVE_BOOK = gql`
-    mutation () {
+// export const REMOVE_BOOK = gql`
+//     mutation () {
         
-    }
-`;
+//     }
+// `;
