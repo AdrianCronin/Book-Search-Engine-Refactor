dd7461d963e10ed85a7caf157d688790415730b0
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-03 17:00:34 -0700

    added empty queries to the mutations.js and filled out the GET_ME query in queries.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,25 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    query () {
+        
+    }
+`;
+
+export const ADD_USER = gql`
+    query () {
+        
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    query () {
+        
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    query () {
+        
+    }
+`;
