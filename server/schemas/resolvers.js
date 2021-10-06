bc20a44752c86c24a7fccc30f8886c8257e979d3
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-05 21:06:42 -0700

    fixed type in resolvers and commented out old SavedBooks code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
 
         me: async (parent, args, context) => {
             if (context.user) {
-                return User.findeOne({ _id: context.user._id });
+                return User.findOne({ _id: context.user._id });
             }
             throw new AuthenticationError('You need to be logged in!');
         },
